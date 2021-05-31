7e342b72484b0197d1454873ffaa541e136630f1
Moritz Steinbusch <moritzsteinbusch@gmail.com>
2021-05-31 11:07:31 +0200

    Added route

---
+++ api/routes/challange.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const mongoose = require('mongoose');
+//const {update} = MongoDB Models
+const router = express.Router();
+//const Fact = MongoDB Models
+
+router.post('/', (req, res, next) => {
+    // TODO
+})
+
+
+
+
+module.exports = router;
--- app.js
+++ app.js
@@ -6,14 +6,50 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
+const challangeRoute = require('./api/routes/challange');
+
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+//mongoose.set('useNewURLParser', true);
+
+const MONGO_URL = process.env.MONGO_URL;
+mongoose.connect(MONGO_URL)
+    .catch(err => {
+        console.error("App starting error: ", err.message);
+        process.exit(1);
+    });
+
+app.use(morgan('dev'));
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if(req.method === 'Options') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
 
 
+app.use('/challange', challangeRoute);
 
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
 
-
-
-
-
-
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
 
 module.exports = app;
