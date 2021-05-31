c73dc7f243a736db7cec3a0292646e3220539c8b
Moritz Steinbusch <moritzsteinbusch@gmail.com>
2021-05-31 13:02:05 +0200

    Added get

--- api/routes/challange.js
+++ api/routes/challange.js
@@ -6,8 +6,19 @@ const ChallangeMong = require('../models/challange');
 
 
 router.get('/', (req, res, next) => {
-    console.log('Hallo');
-})
+    ChallangeMong.find()
+    .exec()
+    .then(docs => {
+        console.log(docs);
+        res.status(200).json(docs);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            err: err
+        });
+    });
+});
 
 
 router.post('/', (req, res, next) => {
