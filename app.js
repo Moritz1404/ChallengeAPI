2048800d601498d09325077ec460394c77b4568e
Moritz Steinbusch <moritzsteinbusch@gmail.com>
2021-05-31 10:40:09 +0200

    Grundgerüst

---
+++ app.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express(); //start express
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+
+
+
+
+
+
+
+
+
+
+module.exports = app;
