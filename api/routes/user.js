c176a2ee4a800011d162c2771f9f3d4b717e370e
Moritz Steinbusch <moritzsteinbusch@gmail.com>
2021-06-03 14:20:45 +0200

    Added: user => /login, middleware => check-auth.js

--- api/routes/user.js
+++ api/routes/user.js
@@ -43,7 +43,49 @@ router.post('/signup', (req, res, next) => {
                 }
             })
         }
+    });
+});
+
+router.post('/login', (req, res, next) => {
+    User.find({ email: req.body.email })
+    .exec()
+    .then(user => {
+        if (user.length < 1) {
+            return res.status(401).json({
+                message: 'Authorization failed'
+            });
+        }
+        bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+            if (err) {
+                return res.status(401).json({
+                    message: 'Authorization failed'
+                });
+            }
+            if (result) {
+                const token = jwt.sign({
+                    email: user[0].email,
+                    userId: user[0]._id
+                },
+                process.env.JWT_KEY,
+                {
+                    expiresIn: "1h"
+                });
+                return res.status(200).json({
+                    message: 'Authorization successful',
+                    token: token
+                });
+            }
+            return res.status(401).json({
+                message: 'Authorization failed'
+            });
+        });
     })
-})
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    });
+});
 
 module.exports = router;
